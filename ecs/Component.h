73fa5c1f001634342faf0f7a3feb0dd03c73e8cb
Luc Bergeron <psyluche@gmail.com>
2019-07-02 22:08:04 -0400

    Erased the old Entity and Component class.

--- ecs/Component.h
+++ ecs/Component.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <string>
-
 namespace ecs {
 
 class Entity;
@@ -10,22 +8,6 @@ class Entity;
 // A generic component class.
 ///////////////////////////////////////////////////////////////////////////////
 
-//class Component
-//{
-//public:
-//    Component() : m_name{""} {}
-//    Component(const std::string& name) : m_name{name} {}
-//    virtual ~Component() {}
-
-//    virtual void TakeTurn(float delta) = 0;
-
-//    const std::string& getName() const { return m_name; }
-
-//    Entity* getEntity() { return m_entity; }
-//protected:
-//    std::string m_name;
-//    Entity* m_entity;
-//};
 
 struct Component
 {
