89146ea5eb68f47681a14720d7f4016d29fa8b2c
Luc Bergeron <psyluche@gmail.com>
2019-07-09 22:08:47 -0400

    Entity is now just a simple struct.
    Added a constant for MAX_ENTITIES.

--- ecs/Entity.h
+++ ecs/Entity.h
@@ -4,9 +4,13 @@
 
 namespace ecs {
 
+//A simple struct with a simple unsigned int id.
 struct Entity {
     std::uint32_t id;
 };
+
+//The maximum number of entities that the system can handle.
+const Entity MAX_ENTITIES = 5000;
 
 } // namespace ecs
 
